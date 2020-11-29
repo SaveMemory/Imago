09d0fdc6e1dfeaff4ddfa8dc63e48d18482e72c4
Filip Lipowiecki <filip.lipowiecki@microsoft.wsei.edu.pl>
2020-11-29 22:45:05 +0100

    Web app initial configuration + dashboard component started

---
+++ VirtualCollege/src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import { DashboardComponent } from './dashboard.component';
+
+
+@NgModule({
+  declarations: [DashboardComponent],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule,
+    MatSidenavModule
+  ]
+})
+export class DashboardModule { }
