a9346882aa2fe258e5d049b183d681603f135427
Filip Lipowiecki <filip.lipowiecki@microsoft.wsei.edu.pl>
2021-04-27 21:16:58 +0200

    renamed project ot Imago

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('VirtualCollege app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Imago app is running!');
   });
 });
